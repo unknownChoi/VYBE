8e0fb2dee512762b0cc8e29889c0105f717f21c4
unknownChoi <justin113013123@gmail.com>
2025-09-26 16:54:37 +0900

    이용내역 수정중

--- lib/features/bottom_nav_passwallet/screens/passwallet_tab_screen.dart
+++ lib/features/bottom_nav_passwallet/screens/passwallet_tab_screen.dart
@@ -13,6 +13,8 @@ enum PassStatus { waiting, entering, entered, reservation }
 
 enum ReservationStatus { pendingApproval, confirmed, canceled }
 
+enum HistoryClubCardReviewStatus { reviewed, notReviewed, expired }
+
 class PasswalletTabScreen extends StatefulWidget {
   const PasswalletTabScreen({super.key});
 
@@ -1016,7 +1018,6 @@ class _BtnSpec {
   _BtnSpec({required this.label, required this.color, required this.onTap});
 }
 
-// ✅ 상황별 라벨·색상 매핑
 List<_BtnSpec> _buttonSpecsFor(PassStatus s, BuildContext context) {
   // 색상은 필요하면 AppColors에 정의해서 교체하세요.
   final Color primary = AppColors.appPurpleColor; // 주요 액션
@@ -1351,7 +1352,11 @@ class ReservationClubCard extends StatelessWidget {
                           ),
                         ),
                         SizedBox(width: 8.w),
-                        SvgPicture.asset('assets/icons/common/dot.svg'),
+                        SvgPicture.asset(
+                          'assets/icons/common/dot.svg',
+                          width: 3.w,
+                          color: Color(0xFFCACACB),
+                        ),
                         SizedBox(width: 8.w),
                         Text(
                           "오후 8:12",
@@ -1365,7 +1370,11 @@ class ReservationClubCard extends StatelessWidget {
                           ),
                         ),
                         SizedBox(width: 8.w),
-                        SvgPicture.asset('assets/icons/common/dot.svg'),
+                        SvgPicture.asset(
+                          'assets/icons/common/dot.svg',
+                          width: 3.w,
+                          color: Color(0xFFCACACB),
+                        ),
                         SizedBox(width: 8.w),
                         Text(
                           "3명",
@@ -1473,10 +1482,260 @@ class HistorySection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "THIS IS HISROTY SECTION",
-        style: TextStyle(color: Colors.white),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HistoryClubCard(reviewStatus: HistoryClubCardReviewStatus.expired),
+          Divider(height: 1.h, thickness: 1.h, color: const Color(0xFF2F2F33)),
+
+          HistoryClubCard(reviewStatus: HistoryClubCardReviewStatus.reviewed),
+          Divider(height: 1.h, thickness: 1.h, color: const Color(0xFF2F2F33)),
+
+          HistoryClubCard(
+            reviewStatus: HistoryClubCardReviewStatus.notReviewed,
+          ),
+          Divider(height: 1.h, thickness: 1.h, color: const Color(0xFF2F2F33)),
+        ],
+      ),
+    );
+  }
+}
+
+class HistoryClubCard extends StatelessWidget {
+  const HistoryClubCard({super.key, required this.reviewStatus});
+
+  final HistoryClubCardReviewStatus reviewStatus;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 20.h),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 18.h,
+                padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 2.h),
+                decoration: BoxDecoration(
+                  color: Color(0xFF535355),
+                  borderRadius: BorderRadius.circular(999.r),
+                ),
+                child: Center(
+                  child: Text(
+                    "현장 웨이팅",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 11.sp,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w600,
+                      height: 1.27,
+                      letterSpacing: -0.55,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 12.h),
+          Row(
+            children: [
+              Container(
+                width: 60.w,
+                height: 60.w,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6.r),
+                ),
+                child: Image.asset('assets/images/test_image/test_1.png'),
+              ),
+              SizedBox(width: 12.w),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "어썸레드",
+                    style: TextStyle(
+                      color: Colors.white /* White */,
+                      fontSize: 24,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w600,
+                      height: 1.08,
+                      letterSpacing: -0.60,
+                    ),
+                  ),
+                  SizedBox(height: 8.h),
+                  Row(
+                    children: [
+                      Text(
+                        "07월 04일 금요일",
+                        style: TextStyle(
+                          color: const Color(0xFFCACACB) /* Gray400 */,
+                          fontSize: 14,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w400,
+                          height: 1.14,
+                          letterSpacing: -0.70,
+                        ),
+                      ),
+                      SizedBox(width: 8.w),
+                      SvgPicture.asset(
+                        'assets/icons/common/dot.svg',
+                        width: 3.w,
+                        color: Color(0xFFCACACB),
+                      ),
+                      SizedBox(width: 8.w),
+                      Text(
+                        "오후 8:12",
+                        style: TextStyle(
+                          color: const Color(0xFFCACACB) /* Gray400 */,
+                          fontSize: 14,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w400,
+                          height: 1.14,
+                          letterSpacing: -0.70,
+                        ),
+                      ),
+                      SizedBox(width: 8.w),
+                      SvgPicture.asset(
+                        'assets/icons/common/dot.svg',
+                        width: 3.w,
+                        color: Color(0xFFCACACB),
+                      ),
+                      SizedBox(width: 8.w),
+                      Text(
+                        "3명",
+                        style: TextStyle(
+                          color: const Color(0xFFCACACB) /* Gray400 */,
+                          fontSize: 14,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w400,
+                          height: 1.14,
+                          letterSpacing: -0.70,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 12.h),
+          if (reviewStatus == HistoryClubCardReviewStatus.notReviewed) ...[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 40.w, vertical: 11.h),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColors.appGreenColor),
+                borderRadius: BorderRadius.circular(6.r),
+              ),
+              child: Column(
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '잊기 전에 ',
+                          style: TextStyle(
+                            color: const Color(0xFFB5FF60) /* Main-Lime500 */,
+                            fontSize: 14,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            height: 1.14,
+                            letterSpacing: -0.35,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '후기를 남겨보세요!',
+                          style: TextStyle(
+                            color: Colors.white /* White */,
+                            fontSize: 14,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            height: 1.14,
+                            letterSpacing: -0.35,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 4.h),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: List.generate(5, (i) {
+                      return Padding(
+                        padding: EdgeInsets.only(right: i == 5 - 1 ? 0 : 8.0),
+                        child: SvgPicture.asset(
+                          'assets/icons/common/star.svg',
+                          width: 29.w,
+                          height: 29.w,
+                          color: Colors.white,
+                        ),
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          ] else if (reviewStatus == HistoryClubCardReviewStatus.expired) ...[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 40.h,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1.w, color: Color(0xFF2F2F33)),
+                borderRadius: BorderRadius.circular(6.r),
+              ),
+              child: Center(
+                child: Text(
+                  '리뷰 작성 기간이 만료되었어요',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: const Color(0xFF707071) /* Gray600 */,
+                    fontSize: 14,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w600,
+                    height: 1.14,
+                    letterSpacing: -0.35,
+                  ),
+                ),
+              ),
+            ),
+          ] else ...[
+            Row(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: List.generate(5, (i) {
+                    return Padding(
+                      padding: EdgeInsets.only(right: i == 5 - 1 ? 0 : 8.0),
+                      child: SvgPicture.asset(
+                        'assets/icons/common/star.svg',
+                        width: 20.w,
+                        height: 20.w,
+                        color: Colors.white,
+                      ),
+                    );
+                  }),
+                ),
+                SizedBox(width: 12.w),
+                Expanded(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 40.h,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 40.w,
+                      vertical: 11.h,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6.r),
+                      border: Border.all(color: Color(0xFF7731FE), width: 1.w),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ],
       ),
     );
   }
